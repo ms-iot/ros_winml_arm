3884cf01bde1794202c780c2a142d827651720b3
Lou Amadio <lamadio@microsoft.com>
2019-02-13 17:10:25 -0800

    Complete-ish demo, add driver app

---
+++ UWPApp/embedded_world/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using RosSharp.RosBridgeClient;
+using std_msgs = RosSharp.RosBridgeClient.Messages.Standard;
+using sensor_msgs = RosSharp.RosBridgeClient.Messages.Sensor;
+using std_srvs = RosSharp.RosBridgeClient.Services.Standard;
+using rosapi = RosSharp.RosBridgeClient.Services.RosApi;
+using rosgraph = RosSharp.RosBridgeClient.Messages.rosgraph;
+
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.Graphics.Imaging;
+using System.Runtime.InteropServices;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace embedded_world
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        [ComImport]
+        [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private unsafe interface IMemoryBufferByteAccess
+        {
+            void GetBuffer(out byte* buffer, out uint capacity);
+        }
+
+        static readonly string uri = "ws://127.0.0.1:9090";
+
+        string imageSubId;
+        string logSubId;
+        RosSocket rosSocket;
+        RosSharp.RosBridgeClient.Protocols.WebSocketUWPProtocol rosWebSocketProtocol;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            rosWebSocketProtocol = new RosSharp.RosBridgeClient.Protocols.WebSocketUWPProtocol(uri);
+            rosSocket = new RosSocket(rosWebSocketProtocol);
+
+            imageSubId = rosSocket.Subscribe<sensor_msgs.Image>("/tracked_objects/image", SubscriptionHandler);
+            logSubId = rosSocket.Subscribe<rosgraph.Log>("/rosout", LogSubscriptionHandler);
+        }
+
+        private void LogSubscriptionHandler(rosgraph.Log message)
+        {
+            var ignore = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                LoggingListBox.Items.Insert(0, new TextBlock() { Text = message.msg});
+            });
+        }
+
+        private void SubscriptionHandler(sensor_msgs.Image message)
+        {
+            var ignore = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                var softwareBitmap = new SoftwareBitmap(BitmapPixelFormat.Bgra8, message.width, message.height, BitmapAlphaMode.Ignore);
+
+                unsafe
+                {
+                    using (BitmapBuffer buffer = softwareBitmap.LockBuffer(BitmapBufferAccessMode.Write))
+                    {
+                        using (var reference = buffer.CreateReference())
+                        {
+                            byte* dataInBytes;
+                            uint capacity;
+                            ((IMemoryBufferByteAccess)reference).GetBuffer(out dataInBytes, out capacity);
+
+                            // Fill-in the BGRA plane
+                            BitmapPlaneDescription bufferLayout = buffer.GetPlaneDescription(0);
+                            for (int y = 0; y < bufferLayout.Height; y++)
+                            {
+                                uint xoffset = 0;
+                                for (int x = 0; x < bufferLayout.Stride; x+=4)
+                                {
+                                    dataInBytes[bufferLayout.StartIndex + bufferLayout.Stride * y + x + 0] = message.data[message.step * y + xoffset + 2];
+                                    dataInBytes[bufferLayout.StartIndex + bufferLayout.Stride * y + x + 1] = message.data[message.step * y + xoffset + 1];
+                                    dataInBytes[bufferLayout.StartIndex + bufferLayout.Stride * y + x + 2] = message.data[message.step * y + xoffset + 0];
+                                    dataInBytes[bufferLayout.StartIndex + bufferLayout.Stride * y + x + 3] = (byte)255;
+
+                                    xoffset += 3;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                var source = new SoftwareBitmapSource();
+                await source.SetBitmapAsync(softwareBitmap);
+
+                MLView.Source = source;
+            });
+        }
+    }
+}
---
+++ k4a_arm_support/src/k4a_arm_support.cpp
@@ -0,0 +1,382 @@
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS 1 // The C++ Standard doesn't provide equivalent non-deprecated functionality yet.
+#pragma comment(lib, "windowsapp")
+
+#include <ros/ros.h>
+#include <cmath>
+#include <visualization_msgs/MarkerArray.h>
+#include <vcruntime.h>
+#include <windows.h>
+#include <winrt/Windows.Foundation.h>
+#include <tf/LinearMath/Quaternion.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <actionlib/client/simple_action_client.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+#include <std_msgs/Int32.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <control_msgs/GripperCommandAction.h>
+
+using namespace std;
+using namespace winrt;
+
+ros::Publisher tracked_object_pub;
+ros::Publisher grip_pub;
+ros::Publisher goto_pub;
+ros::Subscriber markerSub;
+ros::Subscriber gotoSub;
+tf::TransformListener* listener;
+
+moveit::planning_interface::MoveGroupInterface* move_group;
+actionlib::SimpleActionClient<control_msgs::GripperCommandAction>* ac;
+
+
+bool fake = true;
+bool enablePlanning = true;
+
+// Engine Block Cylindars
+// x = 222mm, 1380mm
+// y = 800mm
+// z = 360mm, 750mm, 1140mm, 1530mm
+
+double x1 = .0222;
+double x2 = .1380;
+double xEngineCenter = 0;
+double yEngineCenter = 0;
+double zEngineCenterFirst = .3;
+double zEngineCenterSecond = .28;
+double y = .08;
+
+double correctionR = M_PI;
+double correctionP = 0.0;
+double correctionY = M_PI / 2.0;
+
+double xEngineWorldCoord = .15;
+double yEngineWorldCoord = 0.0;
+double zEngineWorldCoord = 0.0;
+
+
+double headLeftOffset = .0945;
+double headRightOffset = .087;
+
+ros::WallTime lastSeen;
+tf::Transform currentGripPose;
+
+void poseForHead(int32_t number, geometry_msgs::Pose& pose)
+{
+	tf::Quaternion q = tf::createQuaternionFromRPY(0, 0, -M_PI / 2.0);
+    pose.position.x = x1;
+    pose.position.y = headLeftOffset;
+    pose.position.z = y;
+	tf::quaternionTFToMsg(q, pose.orientation);
+}
+
+// yuck
+typedef enum
+{
+	Mesh,
+	XArrow,
+	YArrow,
+	ZArrow
+} MarkerInitType;
+
+void initMarker(visualization_msgs::Marker& marker, std::string name, int32_t id, MarkerInitType type, double x = 0.0, double y = 0.0, double z = 0.0)
+{
+	marker.header.frame_id = "world";
+    marker.text = name;
+	marker.header.stamp = ros::Time();
+	marker.ns = "k4a_arm_support";
+	marker.id = id;
+	marker.type = (type == MarkerInitType::Mesh)?visualization_msgs::Marker::MESH_RESOURCE:visualization_msgs::Marker::ARROW;
+	marker.action = visualization_msgs::Marker::ADD;
+	marker.lifetime = ros::Duration();
+	marker.mesh_use_embedded_materials = true;
+	marker.pose.position.x = x;
+	marker.pose.position.y = y;
+	marker.pose.position.z = z;
+	marker.pose.orientation.x = 0.0;
+	marker.pose.orientation.y = 0.0;
+	marker.pose.orientation.z = 0.0;
+	marker.pose.orientation.w = 1.0;
+
+	marker.points.clear();
+	
+	marker.color.a = 1.0;
+	marker.color.r = 0.0; 	marker.color.g = 0.0;	marker.color.b = 0.0;
+	if (type != MarkerInitType::Mesh)
+	{
+		geometry_msgs::Point pt;
+		marker.points.push_back(pt);
+		switch (type)
+		{
+			case MarkerInitType::ZArrow:
+				marker.color.b = 1.0;
+				pt.z = .1;
+				break;
+
+			case MarkerInitType::YArrow:
+				marker.color.g = 1.0;
+				pt.y = .1;
+				break;
+
+			case MarkerInitType::XArrow:
+				marker.color.r = 1.0;
+				pt.x = .1;
+				break;
+		}
+		marker.points.push_back(pt);
+
+		marker.scale.x = .01;
+		marker.scale.y = .012;
+		marker.scale.z = 0.0;
+	}
+	else
+	{
+        marker.mesh_resource = "package://k4a_arm_support/meshes/Engine_Block.dae";
+		marker.scale.x = .001;
+		marker.scale.y = .001;
+		marker.scale.z = .001;
+		marker.color.r = 0.0;
+		marker.color.g = 0.0;
+		marker.color.b = 1.0;
+	}
+
+}
+
+void openGripper()
+{
+  control_msgs::GripperCommandGoal goal;
+  goal.command.position = 0.08;
+  goal.command.max_effort = 30.0;
+  ac->sendGoal(goal);
+}
+
+void closeGripper()
+{
+  control_msgs::GripperCommandGoal goal;
+  goal.command.position = -0.017;
+  goal.command.max_effort = 30.0;
+  ac->sendGoal(goal);
+  ac->waitForResult(ros::Duration(30.0));
+}
+
+
+void engineMarkerCallback(const visualization_msgs::MarkerArray::ConstPtr& msg)
+{
+    auto foundMarker = *msg->markers.begin();
+
+	if (!listener->waitForTransform ("world", foundMarker.header.frame_id, ros::Time(0), ros::Duration(.1)))
+	{
+		// not ready yet
+		return;
+	}
+
+
+	tf::Transform enginePose;
+	tf::poseMsgToTF(foundMarker.pose, enginePose);
+
+	tf::Vector3 gripOffset(xEngineCenter, yEngineCenter, zEngineCenterFirst);
+
+	tf::StampedTransform engineToWorld;
+	listener->lookupTransform("world", foundMarker.header.frame_id, ros::Time(0), engineToWorld);
+
+	auto engineGripPose = (tf::Transform)engineToWorld * enginePose;
+
+	engineGripPose.getOrigin() += gripOffset;
+
+	tf::Quaternion modelCorrection = tf::createQuaternionFromRPY(correctionR, correctionP, correctionY);
+
+	auto gripRotationQ = engineGripPose * modelCorrection;
+	gripRotationQ.normalize();
+
+
+
+
+    std::vector<visualization_msgs::Marker> markers;
+    visualization_msgs::Marker gripPose;
+    initMarker(gripPose, "gripPoseX", 0, MarkerInitType::XArrow, engineGripPose.getOrigin().x(), engineGripPose.getOrigin().y(), engineGripPose.getOrigin().z());
+	tf::quaternionTFToMsg(gripRotationQ, gripPose.pose.orientation);
+	markers.push_back(gripPose);
+    initMarker(gripPose, "gripPoseY", 1, MarkerInitType::YArrow, engineGripPose.getOrigin().x(), engineGripPose.getOrigin().y(), engineGripPose.getOrigin().z());
+	tf::quaternionTFToMsg(gripRotationQ, gripPose.pose.orientation);
+	markers.push_back(gripPose);
+    initMarker(gripPose, "gripPoseZ", 2, MarkerInitType::ZArrow, engineGripPose.getOrigin().x(), engineGripPose.getOrigin().y(), engineGripPose.getOrigin().z());
+	tf::quaternionTFToMsg(gripRotationQ, gripPose.pose.orientation);
+	markers.push_back(gripPose);
+	grip_pub.publish(markers);
+
+	currentGripPose = engineGripPose;
+	currentGripPose.setRotation(gripRotationQ);
+
+	lastSeen = ros::WallTime::now();
+}
+
+void publishEngine()
+{
+	std::vector<visualization_msgs::Marker> markers;
+    visualization_msgs::Marker marker;
+
+	tf::Quaternion modelCorrection = tf::createQuaternionFromRPY(0, 0, 0);
+	modelCorrection.normalize();
+
+	initMarker(marker, "engine", 0, MarkerInitType::Mesh, xEngineWorldCoord, yEngineWorldCoord, zEngineWorldCoord);
+	tf::quaternionTFToMsg(modelCorrection, marker.pose.orientation);
+	markers.push_back(marker);
+
+	tracked_object_pub.publish(markers);
+}
+
+void timerCallback(const ros::TimerEvent&)
+{
+	publishEngine();
+}
+
+void gotoCallback(const std_msgs::Int32::ConstPtr& msg)
+{
+	if (msg->data == 0)
+	{
+		openGripper();
+		move_group->setNamedTarget("home");
+		move_group->move();
+		return;
+	}
+	else if (msg->data == 2)
+	{
+		closeGripper();
+		return;
+	}
+	else if (msg->data == 3)
+	{
+		openGripper();
+		return;
+	}
+	else if (msg->data == 4)
+	{
+		move_group->setNamedTarget("home");
+		move_group->move();
+		return;
+	}
+
+	if (ros::WallTime::now() > (lastSeen + ros::WallDuration(30, 0)))
+	{
+		// Didn't see soon enough
+		return;
+	}
+
+	move_group->setPoseReferenceFrame("world");
+	move_group->setGoalTolerance(.005);
+
+	geometry_msgs::Pose gripPoseMsg;
+	tf::poseTFToMsg(currentGripPose, gripPoseMsg);
+
+	move_group->setPoseTarget(gripPoseMsg, "ee_link");
+
+	moveit::planning_interface::MoveGroupInterface::Plan gripPlan;
+
+	bool planned = (move_group->plan(gripPlan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+	ROS_INFO_NAMED("k4a", "plan 1 (pose goal) %s", planned ? "" : "FAILED");
+
+	if (!planned)
+	{
+		// ends up in collision with self, so flip 180. If that doesn't work, bail.
+		// rotate the pose
+
+		tf::Quaternion rotateGrip = tf::createQuaternionFromRPY(0, 0, M_PI / 2.0);
+
+		currentGripPose.setRotation(currentGripPose.getRotation() * rotateGrip);
+
+		tf::poseTFToMsg(currentGripPose, gripPoseMsg);
+		move_group->setPoseTarget(gripPoseMsg, "ee_link");
+		planned = (move_group->plan(gripPlan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+		ROS_INFO_NAMED("k4a", "plan 2 (pose goal) %s", planned ? "" : "FAILED");
+	}
+
+	if (planned)
+	{
+		move_group->move();
+	}
+
+	gripPoseMsg.position.z = zEngineCenterSecond;
+
+	planned = move_group->setPoseTarget(gripPoseMsg, "ee_link");
+	planned = (move_group->plan(gripPlan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+	ROS_INFO_NAMED("k4a", "grip (pose goal) %s", planned ? "" : "FAILED");
+
+	if (planned)
+	{
+		move_group->move();
+		closeGripper();
+	}
+}
+
+int main(int argc, char **argv)
+{
+	/*
+    while (!IsDebuggerPresent())
+    {
+        Sleep(5);
+    }
+	*/
+
+    winrt::init_apartment();
+    ros::init(argc, argv, "k4a_arm_support");
+
+    ros::NodeHandle nh;
+    ros::NodeHandle nhPrivate("~");
+    ros::Timer timer;
+    ros::AsyncSpinner async_spinner (2);
+    async_spinner.start();
+
+	lastSeen = ros::WallTime::now() - ros::WallDuration(60);	// last seen while ago
+
+	std::string gripper_name;
+  	nhPrivate.param<std::string>("gripper_name", gripper_name, "gripper");
+    nhPrivate.param<bool>("fake", fake, false);
+	nhPrivate.param<bool>("enablePlanning", enablePlanning, true);
+
+
+	// create the action client
+  	// true causes the client to spin its own thread
+  	ac = new actionlib::SimpleActionClient<control_msgs::GripperCommandAction>(gripper_name, true);
+    ac->waitForServer(); //will wait for infinite time
+
+	listener = new tf::TransformListener();
+
+	grip_pub = nh.advertise<visualization_msgs::MarkerArray>("grip_pose", 1);
+	goto_pub = nh.advertise<std_msgs::Int32>("goto", 1);
+
+	markerSub = nh.subscribe("tracked_objects", 1, engineMarkerCallback);
+
+    gotoSub = nh.subscribe("goto", 1, gotoCallback);
+
+	if (enablePlanning)
+	{
+		moveit::planning_interface::MoveGroupInterface::Options options("arm", "robot_description", nh);
+		move_group = new moveit::planning_interface::MoveGroupInterface(options);
+		geometry_msgs::PoseStamped current_pose = move_group->getCurrentPose();
+
+
+		/*
+		ros::Duration(5.0).sleep();
+		move_group->setNamedTarget("home");
+		move_group->move();
+		*/
+	}
+
+	if (fake)
+	{
+		timer = nh.createTimer(ros::Duration(1), timerCallback);
+		tracked_object_pub = nh.advertise<visualization_msgs::MarkerArray>("tracked_objects", 1);
+
+		std_msgs::Int32 dummy;
+		goto_pub.publish(dummy);
+	}
+
+    ros::waitForShutdown();
+
+    return 0;
+}
